1b07610d92bdf0027775f5655bdd67ef98bfeacf
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2019-09-15 09:05:18 -0700

    First commit

---
+++ src/test/java/guru/springframework/webfluxrest/WebfluxRestApplicationTests.java
@@ -0,0 +1,16 @@
+package guru.springframework.webfluxrest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebfluxRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
