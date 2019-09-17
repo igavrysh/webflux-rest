aaa2d8b17a8d8f6af947f6a0b02435c059d26abd
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2019-09-16 21:33:33 -0700

    Add reactive update category

--- src/test/java/guru/springframework/webfluxrest/controllers/CategoryControllerTest.java
+++ src/test/java/guru/springframework/webfluxrest/controllers/CategoryControllerTest.java
@@ -66,4 +66,19 @@ public class CategoryControllerTest {
                 .expectStatus()
                 .isCreated();
     }
+
+    @Test
+    public void testUpdateCategory() {
+        BDDMockito.given(categoryRepository.save(any(Category.class)))
+                .willReturn(Mono.just(Category.builder().build()));
+
+        Mono<Category> catToUpdateMono = Mono.just(Category.builder().description("Some Cat").build());
+
+        webTestClient.put()
+                .uri("/api/v1/categories/asfdsaf")
+                .body(catToUpdateMono, Category.class)
+                .exchange()
+                .expectStatus()
+                .isOk();
+    }
 }
