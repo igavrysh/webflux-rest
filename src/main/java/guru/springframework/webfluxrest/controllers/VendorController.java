7d1bbd6a06fc946e0bbf12eb36ab174c1d864593
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2019-09-16 21:08:22 -0700

    Add two vendor endpoints

---
+++ src/main/java/guru/springframework/webfluxrest/controllers/VendorController.java
@@ -0,0 +1,31 @@
+package guru.springframework.webfluxrest.controllers;
+
+import guru.springframework.webfluxrest.domain.Category;
+import guru.springframework.webfluxrest.domain.Vendor;
+import guru.springframework.webfluxrest.repositories.CategoryRepository;
+import guru.springframework.webfluxrest.repositories.VendorRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class VendorController {
+
+    private final VendorRepository vendorRepository;
+
+    public VendorController(VendorRepository vendorRepository) {
+        this.vendorRepository = vendorRepository;
+    }
+
+    @GetMapping("/api/v1/vendors")
+    Flux<Vendor> list() {
+        return vendorRepository.findAll();
+    }
+
+    @GetMapping("/api/v1/vendors/{id}")
+    Mono<Vendor> getById(@PathVariable String id) {
+        return vendorRepository.findById(id);
+    }
+}
