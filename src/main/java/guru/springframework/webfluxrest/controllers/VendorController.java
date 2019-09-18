fefc24df18d14455228325ef6fbae475d3a5b9b9
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2019-09-17 21:58:50 -0700

    Add reactive vendor creation

--- src/main/java/guru/springframework/webfluxrest/controllers/VendorController.java
+++ src/main/java/guru/springframework/webfluxrest/controllers/VendorController.java
@@ -4,9 +4,9 @@ import guru.springframework.webfluxrest.domain.Category;
 import guru.springframework.webfluxrest.domain.Vendor;
 import guru.springframework.webfluxrest.repositories.CategoryRepository;
 import guru.springframework.webfluxrest.repositories.VendorRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.reactivestreams.Publisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -27,5 +27,11 @@ public class VendorController {
     @GetMapping("/api/v1/vendors/{id}")
     Mono<Vendor> getById(@PathVariable String id) {
         return vendorRepository.findById(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/api/v1/vendors")
+    Mono<Void> create(@RequestBody Publisher<Vendor> vendorStream) {
+        return vendorRepository.saveAll(vendorStream).then();
     }
 }
--- src/test/java/guru/springframework/webfluxrest/controllers/VendorControllerTest.java
+++ src/test/java/guru/springframework/webfluxrest/controllers/VendorControllerTest.java
@@ -1,16 +1,19 @@
 package guru.springframework.webfluxrest.controllers;
 
+import guru.springframework.webfluxrest.domain.Category;
 import guru.springframework.webfluxrest.domain.Vendor;
 import guru.springframework.webfluxrest.repositories.VendorRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
 
 public class VendorControllerTest {
 
@@ -47,5 +50,20 @@ public class VendorControllerTest {
                 .uri("/api/v1/vendors/someId")
                 .exchange()
                 .expectBody(Vendor.class);
+    }
+
+    @Test
+    public void testCreateVendor() {
+        BDDMockito.given(vendorRepository.saveAll(any(Publisher.class)))
+                .willReturn(Flux.just(Vendor.builder().build()));
+
+        Mono<Vendor> vendorToSaveMono = Mono.just(Vendor.builder().firstName("First Name for Vendor").build());
+
+        webTestClient.post()
+                .uri("/api/v1/vendors")
+                .body(vendorToSaveMono, Vendor.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
     }
 }
